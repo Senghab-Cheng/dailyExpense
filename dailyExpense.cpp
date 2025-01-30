f50540df64dd095a9d69458653f6a1bbede8e561
Senghab-Cheng <scheng@paragoniu.edu.kh>
2025-01-30 18:24:11 +0700

    new push

--- dailyExpense.cpp
+++ dailyExpense.cpp
@@ -1,70 +1,93 @@
-// // Daily Expense Tracker
-// // Create a program to allow users input daily expense,
-// // analyze the data, and display the data in summary by
-// // including:
-// // Total Expense: Sum all expenses.
-// // Average Expense: Divide the total by the number of
-// // days.
-// // Highest Expense
-// // Lowest Expense
+
+#include <iostream>
+#include <string>
+
+#define setprecision
+
+using namespace std;
+
+#define LIGHT_PEACH "\033[38;5;223m"
+#define RESET "\033[0m"
+#define LIGHT_GREEN "\033[92m"
+
+double HIGHEST(double arr[], int size) {
+    double highest = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > highest) {
+            highest = arr[i];
+        }
+    }
+    return highest;
+}
+
+double LOWEST(double arr[], int size) {
+    double lowest = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < lowest) {
+            lowest = arr[i];
+        }
+    }
+    return lowest;
+}
+
+double SUM(double arr[], int size) {
+    double sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
 
 
+double AVERAGE(double sum, int size) {
+    return sum / size;
+}
 
-// #include <iostream>
-// #include <string> 
+void titleOfDailyExpend(double arr[], string days[], int totalOptions) {
+    cout << "===============================================+" << endl;
+    cout << "|                                              |" << endl;
+    cout << "|" << LIGHT_PEACH;
+    cout << "        >>> DAILY EXPEND SYSTEM <<<           " << RESET;
+    cout << "|" << endl;
+    cout << "|                                              |" << endl;
+    cout << "+==============================================+" << endl;
+    cout << "+==============================================+" << endl;
+    cout << "|    Day              |            Spend       |" << endl;
+    cout << "+==============================================+" << endl;
 
-// using namespace std;
-// #define LIGHT_PEACH "\033[38;5;223m"
-// #define RESET "\033[0m" 
-// #define LIGHT_GREEN "\033[92m"
-
-// enum KEY { UP = 72, DOWN = 80, ENTER = 13 }; // Arrow keys and Enter key
-// void setConsoleTextColor(int color) {
-//     // SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-// }
-
-
-
-// void titileOfDailyExpend(int currentSelection, int totalOptions) { cout << "===============================================+" << endl;
-//     cout << "|                                                                                                 |" << endl;
-//     cout << "|"; cout << LIGHT_PEACH; 
-//     cout << "                                   >>> DAILY EXPEND SYSTEM <<<                                     ";cout << RESET;
-//     cout << "|" << endl;
-//     cout << "|                                                                                                 |" << endl;
-//     cout << "+=================================================================================================+" << endl;
-//     cout << "|                                                                                                 |" << endl;
-   
-//          string options[] = {
-//         "TotalExpense",
-//         "averageExpense",
-//         "HighestExpense",
-//         "lowestExpense";
-//     };
-//     for (int i = 0; i < totalOptions; i++) {
-//             if (i == currentSelection) {
-//                 setConsoleTextColor(15); // Bold (White text on black background)
-//                 cout << "|  =>    " << options[i] << string(62 - options[i].length(), ' ') << "                           |" << endl;
-//             } if{
-//                 setConsoleTextColor(8); // Normal (Gray text on black background)
-//                 cout << "|        " << options[i] << string(62 - options[i].length(), ' ') << "                           |" << endl;
-//     }if
-//         }
-
-//     setConsoleTextColor(7);
-//     cout << "|                                                                                                 |" << endl;
-//     cout << "+=================================================================================================+" << endl;
-//     cout << "|";cout << LIGHT_GREEN;
-//     cout << "                          >>>  Select an option by pressing Enter  <<<                           " cout << RESET;
-//     cout << "|" << endl;
-//     cout << "+=================================================================================================+" << endl;
-
-
-//     void TotalEx
+    for (int i = 0; i < totalOptions; i++) {
+        cout << "|     " << days[i] << string(15 - days[i].length(), ' ')
+             << "|       $" << fixed << setprecision(0) << arr[i] << "        |\t " << endl;
+    }
     
-// }
-// int main(){
-//     system("cls");  
-//     // titileOfDailyExpend(sint currentSelection, int totalOption);
-//     string days;
-//     return 0;
-// }
+    cout << "+==============================================+" << endl;
+    cout << "|" << LIGHT_GREEN;
+    cout << "   >>>  Daily Expense Summary <<< " << RESET;
+    cout << "|         " << endl;
+    cout << "+==============================================+" << endl;
+}
+
+
+int main() {
+    system("clear");
+    double arr[7];
+    string DAY[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    
+    for (int i = 0; i < 7; i++) {
+        cout << "On " << DAY[i] << " Total spend of this day: $";
+        cin >> arr[i];
+    }
+    
+    titleOfDailyExpend(arr, DAY, 7);
+      cout << "+============================================+" << endl;
+      cout << "| "<< "The Hightest Expense: "<<  HIGHEST(arr, 7)<< "\t\t|" << endl;
+      cout << "| "<< "The Lowest   Expense: "<<  LOWEST(arr, 7)<< " |" << endl;
+      cout << "| "<< "The Average  Expense: "<<  AVERAGE       << " |" << endl;
+      cout << "| "<< "The Average  Expense: "<<  SUM(arr, 7)   << " |" << endl;
+      
+
+      cout << "+=============================================+" << endl;
+    
+    
+    return 0;
+}
