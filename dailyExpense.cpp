73a90e40664166b34ec2719cf2eebfbad7b65aa9
Senghab-Cheng <scheng@paragoniu.edu.kh>
2025-02-02 14:25:32 +0700

    new push

--- dailyExpense.cpp
+++ dailyExpense.cpp
@@ -1,8 +1,6 @@
-
 #include <iostream>
 #include <string>
-
-#define setprecision
+#include <iomanip>
 
 using namespace std;
 
@@ -38,7 +36,6 @@ double SUM(double arr[], int size) {
     return sum;
 }
 
-
 double AVERAGE(double sum, int size) {
     return sum / size;
 }
@@ -52,42 +49,90 @@ void titleOfDailyExpend(double arr[], string days[], int totalOptions) {
     cout << "|                                              |" << endl;
     cout << "+==============================================+" << endl;
     cout << "+==============================================+" << endl;
-    cout << "|    Day              |            Spend       |" << endl;
+    cout << "|    Day             |            Spend        |" << endl;
     cout << "+==============================================+" << endl;
 
     for (int i = 0; i < totalOptions; i++) {
         cout << "|     " << days[i] << string(15 - days[i].length(), ' ')
-             << "|       $" << fixed << setprecision(0) << arr[i] << "        |\t " << endl;
+             << "|       $" << fixed << setprecision(2) << arr[i] << endl;
     }
-    
-    cout << "+==============================================+" << endl;
-    cout << "|" << LIGHT_GREEN;
-    cout << "   >>>  Daily Expense Summary <<< " << RESET;
-    cout << "|         " << endl;
-    cout << "+==============================================+" << endl;
 }
 
-
 int main() {
+    system("cls");
     system("clear");
     double arr[7];
     string DAY[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-    
+    int option;
+    char anw;
+
     for (int i = 0; i < 7; i++) {
         cout << "On " << DAY[i] << " Total spend of this day: $";
         cin >> arr[i];
     }
-    
-    titleOfDailyExpend(arr, DAY, 7);
-      cout << "+============================================+" << endl;
-      cout << "| "<< "The Hightest Expense: "<<  HIGHEST(arr, 7)<< "\t\t|" << endl;
-      cout << "| "<< "The Lowest   Expense: "<<  LOWEST(arr, 7)<< " |" << endl;
-      cout << "| "<< "The Average  Expense: "<<  AVERAGE       << " |" << endl;
-      cout << "| "<< "The Average  Expense: "<<  SUM(arr, 7)   << " |" << endl;
-      
 
-      cout << "+=============================================+" << endl;
-    
-    
+    titleOfDailyExpend(arr, DAY, 7);
+
+    double highest = HIGHEST(arr, 7);
+    double lowest = LOWEST(arr, 7);
+    double sum = SUM(arr, 7);
+    double average = AVERAGE(sum, 7);
+
+    do {
+        cout << "+==============================================+" << endl;
+        cout << "|" << LIGHT_GREEN;
+        cout << "      >>>  Daily Expense Summary <<< " << RESET;
+        cout << "         |         " << endl;
+        cout << "+==============================================+" << endl;
+        cout << "1. Total expenses." << endl;
+        cout << "2. Average expense." << endl;
+        cout << "3. Highest expense." << endl;
+        cout << "4. Lowest expense. " << endl;
+        cout << "5. All of the above. " << endl;
+        cout << "Select an option: ";
+        cin >> option;
+                switch (option) {
+            case 1:
+                cout << "+==============================================+" << endl;
+                cout << " Total Expense" << "\t" << "           $" << fixed << setprecision(2) << sum << endl;
+                cout << "+==============================================+" << endl;
+                break;
+            case 2:
+                cout << "+==============================================+" << endl;
+                cout << " Average Expense" << "\t" << "  $ " << fixed << setprecision(2) << average << endl;
+                cout << "+==============================================+" << endl;
+                break;
+            case 3:
+                cout << "+==============================================+" << endl;
+                cout << " Highest Expense" << "\t" << "   $" << fixed << setprecision(2) << highest << endl;
+                cout << "+==============================================+" << endl;
+                break;
+            case 4:
+                cout << "+==============================================+" << endl;
+                cout << " Lowest Expense" << "\t" << "   $" << fixed << setprecision(2) << lowest << endl;
+                cout << "+==============================================+" << endl;
+                break;
+            case 5:
+                cout << "+==============================================+" << endl;
+                cout << " Total Expense" << "\t" << "           $" << fixed << setprecision(2) << sum << endl;
+                cout << " Average Expense" << "\t" << "   $" << fixed << setprecision(2) << average << endl;
+                cout << " Highest Expense" << "\t" << "   $" << fixed << setprecision(2) << highest << endl;
+                cout << " Lowest Expense" << "\t" << "        $" << fixed << setprecision(2) << lowest << endl;
+                cout << "+==============================================+" << endl;
+                break;
+            default:
+                cout << "Try again" << endl;
+        }
+
+        if (option >= 1 && option <= 5) {
+            cout << "Do you want to check another option? (y/n): ";
+            cin >> anw;
+        } else {
+            cout << endl;
+        }
+        system("cls");
+
+    } while (anw == 'y' || (option > 6 && option < 0));
+
     return 0;
 }
